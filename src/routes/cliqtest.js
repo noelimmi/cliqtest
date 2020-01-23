29289fc090fd81c64f5e94e5dee452ae5e28024d
Immanuel Irwin <immanuel-pt3486@csez.zohocorpin.com>
2020-01-23 15:59:32 +0530

    Added test post req

--- src/routes/cliqtest.js
+++ src/routes/cliqtest.js
@@ -7,4 +7,8 @@ router.get("/", (req, res) => {
   res.json({ text: "This is response from my own server." });
 });
 
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.json({ text: "This is response from my own server." });
+});
 export default router;
