92858ab317745e29945daa789de18abd99d5c78b
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-04-24 13:39:35 +0530

    Done route changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import middleware from "./middleware";
-import cliqtest from "./routes/cliqtest";
+import cliq from "./routes/cliq";
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,6 +8,6 @@ const app = express();
 middleware(app);
 
 //Routes
-app.use("/cliqtest", cliqtest);
+app.use("/cliq", cliq);
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
