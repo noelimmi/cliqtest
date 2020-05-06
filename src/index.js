ef618d765dc87db3641bbb662a984bb83238c9c7
Immanuel Irwin <noelimmanuel1998@gmail.com>
2020-05-06 20:31:28 +0530

    Added React Front end

--- src/index.js
+++ src/index.js
@@ -11,10 +11,10 @@ middleware(app);
 //Routes
 app.use("/cliq", cliq);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
